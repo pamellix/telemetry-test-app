d900396c518f6ef57eb84047676b460fdeaf130c
Paul TGnkin <paultagankin@icloud.com>
2025-02-18 12:39:51 +0300

    fixed lint errors

--- components/NewsItem.tsx
+++ components/NewsItem.tsx
@@ -1,8 +1,9 @@
+import Image from 'next/image';
 import React from 'react';
 
-export const NewsItem = ({ guid, pictureSet, title, description, position }: Record<string, any>) => (
+export const NewsItem = ({ guid, pictureSet, title, description, position }: Record<string, string>) => (
     <div key={guid}>
-        <img src={pictureSet} alt={title} />
+        <Image src={pictureSet} alt={title} />
         <h4>{title}</h4>
         <p>{description}</p>
         <p>Position: {position}</p>
