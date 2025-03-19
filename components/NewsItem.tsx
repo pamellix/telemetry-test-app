c44a21cd5bedbc1cc6e2c815550ce41307df7c87
pitagankin <paultagankin@icloud.com>
2025-03-19 17:13:57 +0300

    Changed readme

--- components/NewsItem.tsx
+++ components/NewsItem.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const NewsItem = ({ guid, pictureSet, title, description, position }: Record<string, string>) => (
     <div key={guid}>
-        <Image src={pictureSet} alt={title} />
+        <Image src={pictureSet} alt={title} width={300} height={200}/>
         <h4>{title}</h4>
         <p>{description}</p>
         <p>Position: {position}</p>
